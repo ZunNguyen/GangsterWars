2edb0e16936429e8e19c049d0d11340fec4b8de3
ZunNguyen <dungthptcg200599@gmail.com>
2024-11-11 11:26:40 +0700

    Working add read csv file for enemies

--- Assets/Resources/DataBaseConfigs/EnemiesConfig/EnemiesConfig.cs
+++ Assets/Resources/DataBaseConfigs/EnemiesConfig/EnemiesConfig.cs
@@ -1,4 +1,6 @@
-﻿using Sirenix.OdinInspector;
+﻿using Resources.CSV;
+using Sirenix.OdinInspector;
+using Sources.Utils.String;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +8,12 @@ using UnityEngine;
 namespace Sources.DataBaseSystem
 {
     [Serializable]
-    public class EnemyInfo
+    public class EnemyInfo : IReadCSVData
     {
         public string Id;
         [PreviewField(100, ObjectFieldAlignment.Center)]
         public GameObject EnemyPrefab;
-        public List<WaveEnemy> WaveEnemies;
+        public List<WaveEnemy> WaveEnemies = new();
         public Dictionary<string, WaveEnemy> WaveEnemyCache { get; private set; } = new();
 
         public WaveEnemy GetWaveEnemy(string id)
@@ -23,6 +25,53 @@ namespace Sources.DataBaseSystem
             }
             return WaveEnemyCache[id];
         }
+
+#if UNITY_EDITOR
+        private const int _startRowIndex = 1;
+        private const int _startIndexColWaveId = 0;
+        private const int _startIndexColDamage = 1;
+        private const int _startIndexColHp = 2;
+        private const int _startIndexColCoinReward = 3;
+        private const int _startIndexColPerChance = 4;
+
+        private string[] _datas;
+        private int _rowCount;
+        private int _columnCount;
+        [Button]
+        public void ReadFile(TextAsset csvFile)
+        {
+            WaveEnemies.Clear();
+            _datas = csvFile.text.Split(new string[] { ",", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] lines = csvFile.text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            _rowCount = lines.Length;
+            _columnCount = _datas.Length / _rowCount;
+
+            for (int row = _startRowIndex; row < _rowCount; row++)
+            {
+                var indexId = row * _columnCount + _startIndexColWaveId;
+                var indexDamage = row * _columnCount + _startIndexColDamage;
+                var indexHp = row * _columnCount + _startIndexColHp;
+                var indexCoinReward = row * _columnCount + _startIndexColCoinReward;
+                var indexPerChance = row * _columnCount + _startIndexColPerChance;
+
+                var coinReward = StringUtils.SeparateString(_datas[indexCoinReward]);
+                var coinRewardMin = int.Parse(coinReward[0]);
+                var coinRewardMax = int.Parse(coinReward[1]);
+
+                var waveEnemy = new WaveEnemy
+                {
+                    Id = _datas[indexId],
+                    Damage = int.Parse(_datas[indexDamage]),
+                    Hp = int.Parse(_datas[indexHp]),
+                    coinReward = new Vector2Int(coinRewardMin, coinRewardMax),
+                    PercentChance = int.Parse(_datas[indexPerChance])
+                };
+
+                WaveEnemies.Add(waveEnemy);
+            }
+        }
+#endif
     }
 
     [Serializable]
