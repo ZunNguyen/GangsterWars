1fc31b502647675b23d6d59a7c323152984eaf80
ZunNguyen <dungthptcg200599@gmail.com>
2024-11-19 17:05:21 +0700

    Working on journey map #1

--- Assets/Game/Screens/JourneyScreen/JourneyScreen.cs
+++ Assets/Game/Screens/JourneyScreen/JourneyScreen.cs
@@ -1,12 +1,4 @@
-using Sources.DataBaseSystem;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 using Sources.UI;
-using Sources.GamePlaySystem.JourneyMap;
-using Sources.Utils.Singleton;
-using System.Threading.Tasks;
 
 namespace Game.Screens.JourneyScreen
 {
