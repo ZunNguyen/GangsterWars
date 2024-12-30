e6193f126de09d952c0a72f4c212242a756357ab
ZunNguyen <dungthptcg200599@gmail.com>
2024-12-30 14:55:06 +0700

    Working on Joytick #1

---
+++ Assets/Game/Cursor/CursorIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.Cursor
+{
+    public class CursorIcon : MonoBehaviour
+    {
+        private Vector3 moveDirection;
+
+        [SerializeField] private Joystick _joystick;
+
+        private void FixedUpdate()
+        {
+            float horizontal = _joystick.Horizontal;
+            float vertical = _joystick.Vertical;
+
+            // Tạo vector di chuyển
+            moveDirection = new Vector3(horizontal, vertical, 0f);
+
+            // Cập nhật vị trí của cursor
+            transform.position += moveDirection * 5f * Time.deltaTime;
+        }
+    }
+}
