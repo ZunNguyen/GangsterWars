b22f7a69c2482d5f157f54b0dad2d379502a876c
ZunNguyen <dungthptcg200599@gmail.com>
2024-12-31 10:56:46 +0700

    Working on Joytick #4

--- Assets/Game/Cursor/CursorClick.cs
+++ Assets/Game/Cursor/CursorClick.cs
@@ -1,3 +1,5 @@
+using Sources.GamePlaySystem.Joystick;
+using Sources.Utils.Singleton;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +8,14 @@ namespace Game.Cursor
 {
     public class CursorClick : MonoBehaviour
     {
+        private JoystickSystem _joystickSystem => Locator<JoystickSystem>.Instance;
+
         [SerializeField] private CursorIconHandler _cursorIconHandler;
+
+        private void Awake()
+        {
+            if (!_joystickSystem.IsUseJoystick) gameObject.SetActive(false);
+        }
 
         public void OnClick()
         {
